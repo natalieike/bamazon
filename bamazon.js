1af9f3fe27dbb7a77d74eb516d28dde40f8f1b7b
Natalie Ike <nataliesenger@Natalies-MBP.fios-router.home>
2017-09-10 15:58:03 -0700

    Add to Cart functionality

--- bamazon.js
+++ bamazon.js
@@ -10,13 +10,27 @@ function Bamazon(){
 	this.productList = [];
 };
 
-//
-Bamazon.prototype.listProducts = function(mode, connection, whichMenuFunc){
+//Pushes products to productList for further reference, outputs product details.
+Bamazon.prototype.listProducts = function(mode, connection, nextFunc){
+	var that = this;
 	connection.query("SELECT * FROM products LEFT JOIN depts on products.dept_id = depts.dept_id ORDER BY products.prod_id", function(err, res){
 		if(err){
+			connection.end();
 			return console.log(err);
 		}
+		that.productList = [];
 		for(var i = 0; i < res.length; i++){
+			var product = {
+				prodId: res[i].prod_id,
+				prodName: res[i].prod_name,
+				description: res[i].prod_descr,
+				retailPrice: res[i].retail_price,
+				wsp: res[i].wsp,
+				stockQty: res[i].stock_qty,
+				numSold: res[i].num_sold,
+				deptName: res[i].dept_name
+			};
+			that.productList.push(product);
 			console.log("Product ID: " + res[i].prod_id);
 			console.log("Product Name: " + res[i].prod_name);
 			console.log("Description: " + res[i].prod_descr);
@@ -30,7 +44,20 @@ Bamazon.prototype.listProducts = function(mode, connection, whichMenuFunc){
 			}
 			console.log("-----------------------------");
 		}
-		whichMenuFunc();
+		nextFunc();
+	});
+};
+
+//Updates the database with new values based on passed data
+Bamazon.prototype.updateProduct = function(updateObj, connection, nextFunc){
+	var that = this;
+	connection.query("UPDATE products SET ?? = ? WHERE prod_id = ?", [updateObj.field, updateObj.value, updateObj.productId], function(err, res){
+		if(err){
+			connection.end();			
+			return console.log(err);
+		}
+		console.log("Complete!");
+		nextFunc();
 	});
 };
 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,6 +3,7 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 //Required custom Module - shared methods for all bamazon scripts
 var Bamazon = require("./bamazon.js");
+var bamazon = new Bamazon();
 
 //Shopping Cart - array of objects to hold items purchased
 var shoppingCart = [];
@@ -49,15 +50,52 @@ var mainMenu = function(){
 
 //Lists products in Customer format (ID, Name, Description, Retail Price)
 var listProdsCustomer = function(){
-	var bamazon = new Bamazon();
 	bamazon.listProducts("customer", connection, mainMenu);
 };
 
-//
+//Adds product to Shopping Cart
+var buyProduct = function(){
+	bamazon.listProducts("customer", connection, function(){
+		inquirer.prompt([
+      {
+      	type: "input",
+      	message: "Please enter the Product ID you would like to buy: ",
+      	name: "product"
+      },
+      {
+      	type: "input",
+      	message: "Quantity: ",
+      	name: "quantity"
+      }
+		]).then(function(res){
+			var prod = parseInt(res.product);
+			var quant = parseInt(res.quantity);
+			var prodIndex = bamazon.productList.findIndex(x => x.prodId === prod);
+			if(isNaN(res.product)){
+				console.log("You need to enter a Product Number.  Please try again.");
+				mainMenu();
+				return;
+			}else if(isNaN(res.quantity)){
+				console.log("You need to enter an amount as a number.  Please try again.");
+				mainMenu();
+				return;
+			}else if(bamazon.productList[prodIndex].stockQty < quant){
+				console.log("There are not enough products in stock.  Please try again.");
+				mainMenu();
+				return;
+			}else {	
+				var newQuant = {productId: prod, field: "stock_qty", value: bamazon.productList[prodIndex].stockQty - quant};
+				shoppingCart.push(bamazon.productList[prodIndex]);
+				bamazon.updateProduct(newQuant, connection, mainMenu);
+			}
+		});
+	});
+};
 
 //Initialize SQL connection and call Main Menu to start
 connection.connect(function(err){
 	if(err){
+		connection.end();
 		return console.log(err);
 	}
 	mainMenu();
